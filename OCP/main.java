d349bfc05e42f2c313038b8071daa3f4e7e4b38a
vannelpro <vannelpro35@gmail.com>
2025-12-27 18:32:53 +0100

    Add main class to calculate area of a rectangle

---
+++ OCP/main.java
@@ -0,0 +1,8 @@
+package ocp;
+
+public class Main {
+    public static void main(String[] args) {
+        Shape shape = new Rectangle(4, 3);
+        System.out.println("Area = '" + shape.calculateArea() + "'");
+    }
+}
