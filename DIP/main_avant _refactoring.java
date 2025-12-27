9334e7b6a6123b79e64e08ad1a9883fdf74488b0
vannelpro <vannelpro35@gmail.com>
2025-12-27 18:24:07 +0100

    Add main class for order processing

---
+++ DIP/main_avant _refactoring.java
@@ -0,0 +1,9 @@
+package dip;
+
+
+public class Main {
+    public static void main(String[] args) {
+        OrderProcessor order = new OrderProcessor();
+        order.processOrder("'Données à sauvegarder'");
+    }
+}
