534274d53f5e92b88ab97da9e9616210d91274e8
vannelpro <vannelpro35@gmail.com>
2026-01-17 12:58:56 +0100

    Update avant_refactoring.java

--- LSP/avant_refactoring.java
+++ LSP/avant_refactoring.java
@@ -1,46 +1,12 @@
-package lsp;
-
-class Rectangle {
-    protected int width;
-    protected int height;
-
-    public void setWidth(int width) {
-        this.width = width;
-    }
-
-    public void setHeight(int height) {
-        this.height = height;
-    }
-
-    public int getArea() {
-        return width * height;
+public class Bird {
+    public void fly() {
+        System.out.println("The bird is flying");
     }
 }
 
-class Square extends Rectangle {
+public class Penguin extends Bird {
     @Override
-    public void setWidth(int width) {
-        super.setWidth(width);
-        super.setHeight(width);
-    }
-
-    @Override
-    public void setHeight(int height) {
-        super.setWidth(height);
-        super.setHeight(height);
-    }
-}
-
-public class Mainlsp {
-    public static void main(String[] args) {
-        Rectangle rectangle = new Rectangle();
-        rectangle.setWidth(5);
-        rectangle.setHeight(4);
-        System.out.println("Aire du Rectagle =" + rectangle.getArea());
-
-        Rectangle rectangle1 = new Square();
-        rectangle1.setWidth(5);
-        rectangle1.setHeight(4);
-        System.out.println("Aire du Carré =" + rectangle1.getArea());
+    public void fly() {
+        throw new UnsupportedOperationException("Penguins can't fly!");
     }
 }
