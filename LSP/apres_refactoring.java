1de95260be0e14a2c9a0c148bc33f855275c7346
vannelpro <vannelpro35@gmail.com>
2026-01-17 12:59:29 +0100

    Update apres_refactoring.java

--- LSP/apres_refactoring.java
+++ LSP/apres_refactoring.java
@@ -1,42 +1,15 @@
-package lsp;
+public class Bird {}
 
-interface Shape {
-    int getArea();
-}
-
-class Square implements Shape {
-    private int side;
-
-    public Square(int side) {
-        this.side = side;
-    }
-
-    @Override
-    public int getArea() {
-        return side * side;
+public class FlyingBird extends Bird {
+    public void fly() {
+        System.out.println("Flying...");
     }
 }
 
-class Rectangle implements Shape {
-    private int width;
-    private int height;
-
-    public Rectangle(int width, int height) {
-        this.width = width;
-        this.height = height;
-    }
-
-    @Override
-    public int getArea() {
-        return width * height;
+public class NonFlyingBird extends Bird {
+    public void walk() {
+        System.out.println("Walking...");
     }
 }
 
-public class Mainlsp2 {
-    public static void main(String[] args) {
-        Shape square = new Square(3);
-        Shape rectangle = new Rectangle(3, 4);
-        System.out.println("Square Area : " + square.getArea());
-        System.out.println("Rectangle Area: " + rectangle.getArea());
-    }
-}
+public class Penguin extends NonFlyingBird {}
