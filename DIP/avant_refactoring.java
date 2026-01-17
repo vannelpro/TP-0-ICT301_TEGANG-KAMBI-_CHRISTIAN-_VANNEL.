24e4b9e68f5e273aa0ca9101c4d428f14c460387
vannelpro <vannelpro35@gmail.com>
2026-01-17 13:08:22 +0100

    Update avant_refactoring.java

--- DIP/avant_refactoring.java
+++ DIP/avant_refactoring.java
@@ -1,22 +1,12 @@
-public class UserManager {
-
-    public void createUser(String name, String email) {
-        System.out.println("User created: " + name);
-        validateEmail(email);
-        saveToDatabase(name, email);
-        sendEmail(email);
-    }
-
-    public void validateEmail(String email) {
-        System.out.println("Validating email...");
-    }
-
-    public void saveToDatabase(String name, String email) {
-        System.out.println("Saving user to database...");
-    }
-
-    public void sendEmail(String email) {
-        System.out.println("Sending welcome email...");
+public class MTNPayment {
+    public void pay() {
+        System.out.println("Paying with MTN");
     }
 }
 
+public class OrderService {
+    public void processOrder() {
+        MTNPayment payment = new MTNPayment();
+        payment.pay();
+    }
+}
