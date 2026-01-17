71f7d8caed5605369be755c6a1ee355fb0c968c4
vannelpro <vannelpro35@gmail.com>
2026-01-17 13:00:19 +0100

    Delete OCP/main.java

--- OCP/main.java
+++
@@ -1,8 +0,0 @@
-package ocp;
-
-public class Main {
-    public static void main(String[] args) {
-        Shape shape = new Rectangle(4, 3);
-        System.out.println("Area = '" + shape.calculateArea() + "'");
-    }
-}
